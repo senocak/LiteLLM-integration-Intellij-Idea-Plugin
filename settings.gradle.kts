2c31f770bf64eeaa7fb8b7213c0dcf788df2b6fc
TURKCELL\TCASENOCAK <anil.senocak@turkcell.com.tr>
2026-09-16 12:01:13 +0300

    chore: rename root project to litellm-integration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
-rootProject.name = "auto-commit-generator"
+rootProject.name = "litellm-integration"
 
 pluginManagement {
     plugins {
